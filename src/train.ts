054549a9f971432e746a35b8866c85d25f445b3c
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-10-10 16:44:22 +0900

    solved MITask-G

---
+++ src/train.ts
@@ -0,0 +1,30 @@
+// getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
+
+function getHighestIndex1(arr: Array<number>) {
+    if (arr.length === 0) {
+        return -1;
+    }
+    let maxValue = arr[0];
+    let maxIndex = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (maxValue < arr[i]) {
+            maxValue = arr[i];
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
+}
+
+function getHighestIndex2(arr: Array<number>) {
+    return arr.indexOf(Math.max(...arr));
+}
+
+console.log("version 1: ", getHighestIndex1([5, 21, 1.2, 21, 8]));
+console.log("version 1: ", getHighestIndex1([-1]));
+console.log("version 1: ", getHighestIndex1([]));
+console.log("version 1: ", getHighestIndex1([-5, -21, 1.2, -201, 8]));
+
+console.log("version 2: ", getHighestIndex2([5, 21, 1.2, 21, 8]));
+console.log("version 2: ", getHighestIndex2([-1]));
+console.log("version 2: ", getHighestIndex2([]));
+console.log("version 2: ", getHighestIndex2([-5, -21, 1.2, -201, 8]));
