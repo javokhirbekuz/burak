b6c668c4ba2d908ef95d4bd63988740b450d3f2b
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-10-20 22:38:24 +0900

    feat: frontend related css, js and image creation

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,15 @@ import mongoose from "mongoose";
 import app from "./app";
 
 mongoose
-    .connect(process.env.MONGO_URL as string, {})
-    .then((data) => {
-        console.log("MongoDB connection succeed");
-        const PORT = process.env.PORT ?? 3003;
-        app.listen(PORT, function () {
-            console.log(`The app is running on the port: ${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.log("Error on connection MongoDB: ", err);
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+      console.info(`The app is running on the port: ${PORT}`);
+      console.info(`Admin project is running on http://localhost:${PORT}`);
     });
+  })
+  .catch((err) => {
+    console.log("Error on connection MongoDB: ", err);
+  });
