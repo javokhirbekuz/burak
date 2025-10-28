6dcaee9040d217afdda39c31bd1f951810321635
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-10-29 00:25:08 +0900

    feat: createNewProduct business logicby restaurant member

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 import ProductService from "../models/Product.service";
+import { ProductInput } from "../libs/types/product";
+import { AdminRequest } from "../libs/types/member";
 
 const productService = new ProductService();
 
 const productController: T = {};
+
+/**   BSSR   **/
 productController.getAllProducts = async (req: Request, res: Response) => {
   try {
     console.log("getAllProducts");
@@ -17,14 +21,30 @@ productController.getAllProducts = async (req: Request, res: Response) => {
   }
 };
 
-productController.createNewProduct = async (req: Request, res: Response) => {
+productController.createNewProduct = async (
+  req: AdminRequest,
+  res: Response
+) => {
   try {
     console.log("createNewProduct");
-    res.send("Done");
+    const files = req.files;
+    if (!req.files?.length)
+      throw new Errors(HttpCode.INTERNAL_SERVER_ERR0R, Message.CREATE_FAILED);
+    const data: ProductInput = req.body;
+    data.productImages = req.files?.map((ele) => ele.path.replace(/\\/g, "/"));
+
+    await productService.createNewProduct(data);
+
+    res.send(
+      `<script>alert("Successfull creation");  window.location.replace('/admin/product/all')</script>`
+    );
   } catch (err) {
     console.log("Error createNewProduct:", err);
-    if (err instanceof Errors) res.status(err.code).json(err);
-    else res.status(Errors.standard.code).json(Errors.standard);
+    const message =
+      err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+    res.send(
+      `<script>alert("${message}");  window.location.replace('/admin/product/all')</script>`
+    );
   }
 };
 
@@ -37,5 +57,7 @@ productController.updateChosenProduct = async (req: Request, res: Response) => {
     else res.status(Errors.standard.code).json(Errors.standard);
   }
 };
+
+/**   SPA   **/
 
 export default productController;
--- src/schema/Product.model.ts
+++ src/schema/Product.model.ts
@@ -37,13 +37,12 @@ const productSchema = new Schema(
     },
 
     productVolume: {
-      type: String,
+      type: Number,
       enum: ProductVolume,
       default: ProductVolume.ONE,
     },
     productDesc: {
       type: String,
-      required: true,
     },
 
     productImages: {
