d01efefc63e2d7b01719eb72b0bdc04e41039f13
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-10-28 23:50:03 +0900

    fix: modify restaurant controller processSignup and processLogin

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -3,7 +3,7 @@ import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enum/member.enum";
-import Errors, { Message } from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 
 const memberService = new MemberService();
 const restaurantController: T = {};
@@ -44,13 +44,17 @@ restaurantController.processSignup = async (
 ) => {
   try {
     console.log("processSignup");
+    const file = req.file;
+    if (!file)
+      throw new Errors(HttpCode.BAD_REQUEST, Message.SOMETHING_WENT_WRONG);
     const newMember: MemberInput = req.body;
+    newMember.memberImage = file?.path;
     newMember.memberType = MemberType.RESTAURANT;
     const result = await memberService.processSignup(newMember);
     //  TODO: SESSIONS AUTHENTICATION
     req.session.member = result;
     req.session.save(function () {
-      res.send(result);
+      res.redirect("/admin/product/all");
     });
   } catch (err) {
     console.log("Error processSignup:", err);
@@ -75,8 +79,7 @@ restaurantController.processLogin = async (
     //  TODO: SESSIONS AUTHENTICATION
     req.session.member = result;
     req.session.save(function () {
-      // console.log()
-      res.send(result);
+      res.redirect("/admin/product/all");
     });
   } catch (err) {
     console.log("Error processLogin:", err);
