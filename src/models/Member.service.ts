97c3764a00831cfe586c8559139b6427eea5a7d1
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-10-29 21:45:43 +0900

    feat: develop getUsers business logic

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -66,6 +66,7 @@ class MemberService {
       throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
     }
   }
+
   public async processLogin(input: LoginInput): Promise<Member> {
     const member = await this.memberModel
       .findOne(
@@ -88,6 +89,14 @@ class MemberService {
     return await this.memberModel.findById(member._id).exec();
     // console.log("member", member);
     // return result;
+  }
+
+  public async getUsers(): Promise<Member[]> {
+    const result = await this.memberModel
+      .find({ memberType: MemberType.USER })
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    return result;
   }
 }
 export default MemberService;
