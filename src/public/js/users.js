963e7921ea3a47ee590b5e8315d9639ef0cdc5d4
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-11-05 00:47:34 +0900

    feat: frontend chosen user update logic

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,29 @@
 console.log("Users frontend javascript file");
+$(function () {
+  $(".member-status").on("change", function (e) {
+    const id = e.target.id;
+    const memberStatus = $(`#${id}.member-status`).val();
+    console.log("id", id);
+    console.log("memberStatus", memberStatus);
+
+    axios
+      .post(`/admin/user/edit`, {
+        _id: id,
+        memberStatus: memberStatus,
+      })
+      .then(() => {
+        console.log("response:", response);
+        const result = response.data;
+        if (result.data) {
+          console.log("user updated");
+          $(".member-status").blur();
+        } else {
+          alert("User Update is failed");
+        }
+      })
+      .catch((err) => {
+        console.log("ERROR", err);
+        alert("User update is failed");
+      });
+  });
+});
