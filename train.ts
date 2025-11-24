df9986292025806c15a02c369d6c6404ce4e770c
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-11-24 11:31:43 +0900

    solve MITask-W

--- train.ts
+++ train.ts
@@ -237,19 +237,45 @@
 // console.log(sumOdds(9));
 // console.log(sumOdds(11));
 
-// MITask-V:
-// Shunday function yozing, uni string parametri bolsin va stringdagi harf va u harf necha marta takrorlangani sonidan tashkil topgan object qaytarsin.
-// MASALAN: countChars("hello") return {h: 1, e: 1, l: 2, o: 1}
-interface T {
-  [key: string]: number;
-}
-function countChars(str: string): T {
-  const result: T = {};
-  for (const letter of str.split("")) {
-    if (!result[letter]) result[letter] = 1;
-    else result[letter] += 1;
+// // MITask-V:
+// // Shunday function yozing, uni string parametri bolsin va stringdagi harf va u harf necha marta takrorlangani sonidan tashkil topgan object qaytarsin.
+// // MASALAN: countChars("hello") return {h: 1, e: 1, l: 2, o: 1}
+// interface T {
+//   [key: string]: number;
+// }
+// function countChars(str: string): T {
+//   const result: T = {};
+//   for (const letter of str.split("")) {
+//     if (!result[letter]) result[letter] = 1;
+//     else result[letter] += 1;
+//   }
+//   return result;
+// }
+
+// console.log(countChars("hello"));
+
+// MITask-W:
+// Shunday function yozing, uni array va number parametrlari bolsin. Function arrayni numberda berilgan uzunlikda kesib bolaklarga ajratilgan array holatida qaytarsin
+// MASALAN: chunkArray([1,2,3,4,5,6,7,8,9,10], 3) return [[1,2,3], [4,5,6], [7,8,9], [10]]
+// const chunkArray = function (arr: number[], n: number): number[][] {
+//   let counter = 0;
+//   const result = [];
+//   while (Math.ceil(arr.length / n) > counter) {
+//     result.push(arr.slice(counter * n, counter * n + n));
+//     counter++;
+//   }
+//   return result;
+// };
+
+const chunkArray = function (arr: number[], n: number) {
+  const result = [];
+  for (let i = 0; i < arr.length; i = i + n) {
+    result.push(arr.slice(i, i + n));
   }
   return result;
-}
+};
+console.log(chunkArray([6, 2, 1, 4, 2, 3, 4, 6, 9], 2));
 
-console.log(countChars("hello"));
+console.log(chunkArray([6, 2, 1], 2));
+
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
