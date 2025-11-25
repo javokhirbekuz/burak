a54f7221f85f2c0adc3b2689f2f8128f729d4bde
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-11-25 23:14:36 +0900

    feat: develop updateMember business logic

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -3,6 +3,7 @@ import {
   LoginInput,
   Member,
   MemberInput,
+  MemberUpdateInput,
 } from "../libs/types/member";
 import { T } from "../libs/types/common";
 import { NextFunction, Request, Response } from "express";
@@ -76,6 +77,21 @@ memberController.getMemberDetail = async (
     res.status(HttpCode.OK).json(result);
   } catch (err) {
     console.log("Error getMemberDetail:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
+memberController.updateMember = async (req: ExtendedRequest, res: Response) => {
+  try {
+    console.log("updateMember");
+
+    const input: MemberUpdateInput = req.body;
+    if (req.file) input.memberImage = req.file.path.replace(/\\/, "/");
+    const result = await memberService.updateMember(req.member, input);
+    res.status(HttpCode.OK).json(result);
+  } catch (err) {
+    console.log("Error updateMember:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
   }
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import memberController from "./controllers/member.controller";
+import makeUploader from "./libs/utils/uploader";
 const router = express.Router();
 
 /**   Member    **/
@@ -15,6 +16,13 @@ router.get(
   "/member/detail",
   memberController.verifyAuth,
   memberController.getMemberDetail
+);
+
+router.post(
+  "/member/update",
+  memberController.verifyAuth,
+  makeUploader("members").single("memberImage"),
+  memberController.updateMember
 );
 
 /**   Product    **/
