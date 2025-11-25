3566618d50dab22cf427f3987b571f807076a631
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-11-25 23:16:38 +0900

    feat: create getRestaurant rest Api

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,7 @@ import makeUploader from "./libs/utils/uploader";
 const router = express.Router();
 
 /**   Member    **/
+router.get("/member/restaurant", memberController.getRestaurant);
 router.post("/member/login", memberController.login);
 router.post("/member/signup", memberController.signup);
 router.post(
