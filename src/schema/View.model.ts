42fa8f11b7d4a44f1d39d049e823166b2686d9a0
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-11-27 20:49:52 +0900

    feat: create viewSchema and service models

---
+++ src/schema/View.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+import { ViewGroup } from "../libs/enum/view.enum";
+
+const viewSchema = new Schema(
+  {
+    viewGroup: {
+      type: String,
+      enum: ViewGroup,
+      required: true,
+    },
+    memberId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Member",
+    },
+    viewRefId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      // ref: "Product",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("View", viewSchema);
