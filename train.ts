c331ccfaf5084a49c086331b8e941aafc19c819f
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-11-29 01:48:40 +0900

    feat: solved MITask-Z

--- train.ts
+++ train.ts
@@ -310,11 +310,19 @@
 //   )
 // );
 
-// MITask-Y:
-// MASALAN: findIntersection([1,2,3], [3,2,0]) return [2,3]
+// // MITask-Y:
+// // MASALAN: findIntersection([1,2,3], [3,2,0]) return [2,3]
 
-function findIntersection(arr1: number[], arr2: number[]): number[] {
-  return arr1.filter((ele) => arr2.includes(ele));
+// function findIntersection(arr1: number[], arr2: number[]): number[] {
+//   return arr1.filter((ele) => arr2.includes(ele));
+// }
+
+// console.log(findIntersection([1, 2, 3], [3, 2, 0]));
+
+// MITask-Z:
+// MASALAN: sumEvens([1,2,3]) return 2
+
+function sumEvens(arr: number[]): number {
+  return arr.reduce((acc, ele) => (ele % 2 === 0 ? acc + ele : acc), 0);
 }
-
-console.log(findIntersection([1, 2, 3], [3, 2, 0]));
+console.log(sumEvens([1, 2, 3, 4, 5, 6, 7, 8, 9]));
